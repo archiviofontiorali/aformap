604fc4d69cfbd42226b38f42a7865292bdd8e8a4
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2021-05-05 01:10:24 +0200

    Create Interview model

--- afor-map/models.py
+++ afor-map/models.py
@@ -15,3 +15,13 @@ class Reference:
     @property
     def has_coordinates(self):
         return (self.latitude is not None) and (self.longitude is not None)
+
+
+@dataclasses.dataclass
+class Interview:
+    id: str
+    video_id: str
+    fullname: str
+    video_ref: str
+    latitude: float = None
+    longitude: float = None
