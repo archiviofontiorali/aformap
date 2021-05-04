f2aa4b571af54ccd928cb27b802a8c6cae788b01
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2021-05-04 22:38:22 +0200

    Add geodecode in script

--- afor-map/__main__.py
+++ afor-map/__main__.py
@@ -56,8 +56,35 @@ class GEOLocator:
         return latitude, longitude
 
 
-nodes_path = Path("data/nodes_fixed.csv")
+DATA_PATH = Path("data")
+OUTPUT_PATH = Path("output")
+
+csv_loader = CSV(delimiter="\t", quotechar='"')
+geo_locator = GEOLocator()
+
+# Read nodes
+nodes_path = DATA_PATH / "nodes_fixed.csv"
 nodes = csv_loader.read(nodes_path, Node)
 
+# Convert address to coordinates
+bar = progress.bar.IncrementalBar("Geocoding...", max=len(nodes))
 for node in nodes:
-    print(node)
+    if node.address:
+        node.latitude, node.longitude = geo_locator.geocode(node.address)
+    bar.next()
+else:
+    bar.finish()
+
+# Save nodes with coordinates
+geocoded_path = OUTPUT_PATH / "nodes_geocoded.csv"
+geocoded_header = [
+    "id",
+    "video_id",
+    "name",
+    "address",
+    "description",
+    "latitude",
+    "longitude",
+]
+geocoded_nodes = filter(lambda n: n.latitude is None or n.longitude is None, nodes)
+csv_loader.write(geocoded_path, geocoded_header, geocoded_nodes)
