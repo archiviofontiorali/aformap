59ac2ec4cb259b5bff962468f0426f0048daf7f8
Elia Giacobazzi <elia.giacobazzi@euler.it>
2021-05-14 17:48:22 +0200

    Refactor: clean up

--- aformap/__main__.py
+++ aformap/__main__.py
@@ -1,5 +1,4 @@
 import dataclasses
-
 from typing import List
 
 import progress.bar
@@ -8,9 +7,12 @@ from .constants import ARCHIVE_CREATOR, DATA_PATH, OUTPUT_PATH
 from .models import Interview, Place
 from .repos import CSV, JSON, InternetArchive
 
+# Ensure input/output folders exists
 DATA_PATH.mkdir(parents=True, exist_ok=True)
 OUTPUT_PATH.mkdir(parents=True, exist_ok=True)
 
+
+# Load repositories
 ia = InternetArchive(creator=ARCHIVE_CREATOR, media_type="movies")
 csv = CSV(delimiter="\t", quotechar='"')
 json = JSON()
@@ -20,13 +22,9 @@ json = JSON()
 legacy_edges_path = DATA_PATH / "edges.csv"
 legacy_edges = csv.read(legacy_edges_path, has_header=True)
 
-assert legacy_edges_path.exists(), legacy_edges_path
-assert isinstance(legacy_edges, list)
-assert len(legacy_edges) > 1
-assert all(isinstance(edge, list) for edge in legacy_edges), legacy_edges
 
+# Parse data inside legacy `edges.csv` file
 interviews: List[Interview] = []
-
 _bar = progress.bar.IncrementalBar("Parsing edges.csv", max=len(legacy_edges))
 for i, edge in enumerate(legacy_edges, 1):
     # When a new identifier is found add a new interview and make active
@@ -39,13 +37,12 @@ for i, edge in enumerate(legacy_edges, 1):
     # Get information about place
     place = Place(title=edge[1], latitude=float(edge[3]), longitude=float(edge[2]))
     interviews[-1].places.append(place)
-
     _bar.next()
 else:
     _bar.finish()
 
 
-# save as json
+# Save as json
 output_path = OUTPUT_PATH / "map-data.json"
 interviews_as_dict = [dataclasses.asdict(interview) for interview in interviews]
 json.write(output_path, interviews_as_dict)
