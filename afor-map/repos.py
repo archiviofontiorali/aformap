b41f6c6864db3cb685e9ef287140a5975573ee08
Elia Giacobazzi <elia.giacobazzi@euler.it>
2021-05-14 16:30:51 +0200

    Improve CSV reader

--- afor-map/repos.py
+++ afor-map/repos.py
@@ -11,12 +11,19 @@ class CSV:
     def __init__(self, **opts):
         self._opts = opts
 
-    def read(self, path, model, skip_header=True) -> list:
+    def read(self, path, has_header=False, converter=None) -> list:
+        """Read a CSV file from disk.
+
+        :param path: a string or path-like object
+        :param has_header: set to True if first row is an header
+        :param converter: a function who take a row and optionally the header
+        """
         with open(path, "rt") as fp:
             reader = csv.reader(fp, **self._opts)
-            if skip_header:
-                next(reader)
-            return [model(*row) for row in reader]
+            header = next(reader) if has_header else None
+            if converter:
+                return [converter(row, header) for row in reader]
+            return list(reader)
 
     def write(self, path, header, objs, write_header=True):
         with open(path, "wt") as fp:
@@ -51,7 +58,6 @@ class InternetArchive:
 
     def fetch_all(self):
         _query = f"creator:{self._creator} mediatype:{self._media_type}"
-        print(_query)
         _items = ia.search_items(_query)
         return [it["identifier"] for it in _items]
 
